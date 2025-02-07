f44a013057d290e50d0d816cf0c41393b1084585
faizkhan24 <khanfaiz33897@gmail.com>
2025-02-07 15:43:54 +0530

    fixing issue in category page.tsx

--- app/[category]/page.tsx
+++ app/[category]/page.tsx
@@ -3,13 +3,14 @@ import { notFound } from "next/navigation";
 import CoursePage from "@/components/Courses/CoursePage";
 import { courseTypes } from "@/utils/courseTypes";
 
-interface Props {
-  params: { category: string };
-  searchParams: { [key: string]: string | string[] | undefined };
-}
+type PageProps = {
+  params: Promise<{ category: string }>;
+  searchParams?: { [key: string]: string | string[] | undefined };
+};
 
-export default async function CategoryPage(props: Props) {
-  const category = props.params.category.toLowerCase();
+export default async function CategoryPage(props: PageProps) {
+  const params = await props.params;
+  const category = params.category.toLowerCase();
   console.log("category:", category);
   
   // Get courses for the specific category
