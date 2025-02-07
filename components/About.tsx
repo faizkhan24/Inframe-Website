2269f538b47b7107640ba787f0647a8a3ce74de6
faizkhan24 <khanfaiz33897@gmail.com>
2025-02-07 16:54:23 +0530

    fixing bugs category

--- components/About.tsx
+++ components/About.tsx
@@ -1,3 +1,4 @@
+'use client'
 import React, { useEffect } from "react";
 import { Card, CardContent } from "@/components/ui/card";
 import { highlights, LOGOS, studentImages, values } from "@/utils/constant";
